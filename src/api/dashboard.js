67b895ea0204ed8f02b6e0b0d3f1165f70ea1eaf
Bilal Hussain <hellobilalhussain@gmail.com>
2025-05-13 16:44:56 +0500

    removed temp data

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,27 +1,23 @@
 import { api } from ".";
-import { historicalData } from "data/historicalData"; // will be removed --------------
-import { data } from "data/index"; // will be removed --------------
 
 export const getStockData = async (range) => {
-  //   const response = await api.get("query", {
-  //     params: {
-  //       function: "TIME_SERIES_INTRADAY",
-  //       symbol: "IBM",
-  //       interval: "5min",
-  //       month: range,
-  //     },
-  //   });
-  //   return response.data;
-  return data; // will be removed --------------------
+    const response = await api.get("query", {
+      params: {
+        function: "TIME_SERIES_INTRADAY",
+        symbol: "IBM",
+        interval: "5min",
+        month: range,
+      },
+    });
+    return response.data;
 };
 
 export const getHistoricalData = async (selectedStock) => {
-    // const response = await api.get("query", {
-    //   params: {
-    //     function: "TIME_SERIES_DAILY",
-    //     symbol: selectedStock,
-    //   },
-    // });
-    // return response.data;
-  return historicalData; // will be removed --------------------
+    const response = await api.get("query", {
+      params: {
+        function: "TIME_SERIES_DAILY",
+        symbol: selectedStock,
+      },
+    });
+    return response.data;
 };
